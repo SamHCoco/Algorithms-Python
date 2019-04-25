2f46237c4264dc9e6a1b85643449a3d690ccda59
Sam Coco <Samuel.HenriAC@outlook.com>
2019-04-25 16:18:46 +0100

    Bisection Search successfully tested

--- src/test_algorithms.py
+++ src/test_algorithms.py
@@ -11,3 +11,11 @@ class TestAlgorithms(unittest.TestCase):
     def test_bubble_sort(self):
         """Tests bubble sort algorithm."""
         self.assertEqual(bubble_sort(self.unordered_list), self.expected_order)
+        self.assertEqual(bubble_sort([]), None)
+
+    def test_bisection_search(self):
+        """Tests bisection search."""
+        self.assertEqual(bisection_search(-4, self.unordered_list), 1)
+        self.assertEqual(bisection_search(764.2, self.unordered_list), 5)
+        self.assertEqual(bisection_search(3.14159, self.unordered_list), None)
+        self.assertEqual(bisection_search(-4, []), None)
